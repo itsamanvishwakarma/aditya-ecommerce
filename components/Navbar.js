b8b516cdea83b759555fea5d13ae28ff680f2804
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2023-02-18 00:27:03 +0530

    order-page-photo_buy-now_add-to-cart-buttons-are-yet-to-debud(buttons-in-mobile-screens)_(photo-in-large-screens)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useRef } from "react";
+import {
+  AiFillCloseCircle,
+  AiFillMinusCircle,
+  AiFillPlusCircle,
+} from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
+import { ImBin } from "react-icons/im";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
   return (
     <>
       <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-lg">
@@ -25,8 +43,96 @@ const Navbar = () => {
             </Link>
           </ul>
         </div>
-        <div className="cart absolute right-0 top-4 mx-5">
-          <AiOutlineShoppingCart className="text-xl md:text-2xl" />
+        <div
+          onClick={toggleCart}
+          className="cursor-pointer cart absolute right-0 top-4 mx-5"
+        >
+          <FiShoppingCart className="text-xl md:text-2xl" />
+        </div>
+        {/* Sidebar Shopping Cart */}
+        <div
+          ref={ref}
+          className="w-72 h-full sideCart absolute top-0 right-0 bg-indigo-100 px-8 py-10 transform transition-transform translate-x-full"
+        >
+          <h1 className="font-bold text-xl text-center">Shopping Cart</h1>
+          <div
+            onClick={toggleCart}
+            className="absolute top-5 right-2 cursor-pointer text-2xl text-indigo-500"
+          >
+            <AiFillCloseCircle />
+          </div>
+          <ol className="list-decimal font-semibold">
+            <li>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  Handpurse - Ladies Hand Purse
+                </div>
+                <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                  <AiFillMinusCircle className="cursor-pointer text-indigo-500" />
+                  <span className="mx-2 text-sm">1</span>
+                  <AiFillPlusCircle className="cursor-pointer text-indigo-500" />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  Handpurse - Ladies Hand Purse
+                </div>
+                <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                  <AiFillMinusCircle className="cursor-pointer text-indigo-500" />
+                  <span className="mx-2 text-sm">1</span>
+                  <AiFillPlusCircle className="cursor-pointer text-indigo-500" />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  Handpurse - Ladies Hand Purse
+                </div>
+                <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                  <AiFillMinusCircle className="cursor-pointer text-indigo-500" />
+                  <span className="mx-2 text-sm">1</span>
+                  <AiFillPlusCircle className="cursor-pointer text-indigo-500" />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  Handpurse - Ladies Hand Purse
+                </div>
+                <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                  <AiFillMinusCircle className="cursor-pointer text-indigo-500" />
+                  <span className="mx-2 text-sm">1</span>
+                  <AiFillPlusCircle className="cursor-pointer text-indigo-500" />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="item flex my-5">
+                <div className="w-2/3 font-semibold">
+                  Handpurse - Ladies Hand Purse
+                </div>
+                <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                  <AiFillMinusCircle className="cursor-pointer text-indigo-500" />
+                  <span className="mx-2 text-sm">1</span>
+                  <AiFillPlusCircle className="cursor-pointer text-indigo-500" />
+                </div>
+              </div>
+            </li>
+          </ol>
+          <div className="flex">
+            <button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+              <BsFillBagCheckFill className="m-1" />
+              Checkout
+            </button>
+            <button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+              <ImBin className="m-1" />
+              Clear Cart
+            </button>
+          </div>
         </div>
       </div>
     </>
---
+++ pages/api/pincode.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res
+    .status(200)
+    .json([
+      110002, 110005, 110003, 110010, 110016, 110018, 110021, 110024, 110082,
+    ]);
+}
