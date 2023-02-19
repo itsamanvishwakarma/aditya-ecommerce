5733781f0bd4fd0258c649709475b69a046ef8ad
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2023-02-19 22:32:15 +0530

    product.map-fucntion-error_nextjs-hydration-error-images-are-getting-squished

--- pages/shoulderBag.js
+++ pages/shoulderBag.js
@@ -7,35 +7,40 @@ import mongoose from "mongoose";
 
 const ShoulderBag = ({ products }) => {
   return (
-    <>
+    <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <Link
-                href={"/product/Stand-out-from-the-crowd"}
-                className="block relative rounded overflow-hidden"
-              >
-                <img
-                  alt="ecommerce"
-                  className="m-auto md:m-0 h-[30vh] md:h-[36vh] block"
-                  src="https://m.media-amazon.com/images/I/41pIShJzyJS._SY395_SX395_.jpg"
-                />
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    SHOULDER BAG
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    Lavie Women's Nova Tote Bag | Ladies Purse Handbag
-                  </h2>
-                  <p className="mt-1">₹499</p>
-                </div>
-              </Link>
-            </div>
+            {products.map((item) => {
+              return (
+                <Link key={item._id} href={"/product/Stand-out-from-the-crowd"}>
+                  <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+                    <a className="block relative rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block"
+                        src="https://m.media-amazon.com/images/I/41pIShJzyJS._SY395_SX395_.jpg"
+                      />
+                    </a>
+                    <div className="mt-4 text-center md:text-left">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        SHOULDER BAG
+                      </h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                        Ladies Purse Handbag
+                      </h2>
+                      <p className="mt-1">₹499</p>
+                      <p className="mt-1">S, M, L, XL, XXL</p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+            ;
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
