e8a27ac78c9a7e6ca907942fd1a80652b34d0110
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2023-02-20 22:37:26 +0530

    logic-behind-size-color-availableqty-build-if-error-consist-in-getproducts.js_use-out-the_commented-code-instead-refectored-code

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -1,9 +1,72 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Product from "@/models/Product";
 import connectDB from "@/middleware/mongoose";
 
 const handler = async (req, res) => {
-  let products = await Product.find();
-  res.status(200).json({ products });
+  try {
+    const products = await Product.find();
+    const shoulderbag = {};
+
+    products.forEach((item) => {
+      if (!shoulderbag[item.title]) {
+        shoulderbag[item.title] = {
+          ...item.toObject(),
+          color: [],
+          size: [],
+        };
+      }
+
+      if (item.availableQty > 0) {
+        if (!shoulderbag[item.title].color.includes(item.color)) {
+          shoulderbag[item.title].color.push(item.color);
+        }
+        if (!shoulderbag[item.title].size.includes(item.size)) {
+          shoulderbag[item.title].size.push(item.size);
+        }
+      }
+    });
+
+    res.status(200).json({ shoulderbag });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
 export default connectDB(handler);
+
+// If above code doesn't work, try this one
+
+// import Product from "@/models/Product";
+// import connectDB from "@/middleware/mongoose";
+
+// const handler = async (req, res) => {
+//   let products = await Product.find();
+//   let shoulderbag = {};
+//   for (let item of products) {
+//     if (item.title in shoulderbag) {
+//       if (
+//         !shoulderbag[item.title].color.includes(item.color) &&
+//         item.availableQty > 0
+//       ) {
+//         shoulderbag[item.title].color.push(item.color);
+//       }
+//       if (
+//         !shoulderbag[item.title].size.includes(item.size) &&
+//         item.availableQty > 0
+//       ) {
+//         shoulderbag[item.title].size.push(item.size);
+//       }
+//     } else {
+//       shoulderbag[item.title] = JSON.parse(JSON.stringify(item));
+//       if (item.availableQty > 0) {
+//         shoulderbag[item.title].color = [item.color];
+//         shoulderbag[item.title].size = [item.size];
+//       } else {
+//         shoulderbag[item.title].color = [];
+//         shoulderbag[item.title].size = [];
+//       }
+//     }
+//   }
+//   res.status(200).json({ shoulderbag });
+// };
+
+// export default connectDB(handler);
--- pages/shoulderBag.js
+++ pages/shoulderBag.js
@@ -11,15 +11,19 @@ const ShoulderBag = ({ products }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="grid lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-1 gap-4">
-            {products.map((item) => {
+            {Object.keys(products).map((item) => {
               return (
-                <Link key={item._id} href={"/product/Stand-out-from-the-crowd"}>
+                <Link
+                  passHref={true}
+                  key={products[item]._id}
+                  href={`/product/${products[item].slug}`}
+                >
                   <div className="lg:w-auto md:w-auto p-4 w-full shadow-lg m-5">
                     <a className="block relative rounded overflow-hidden">
                       <img
                         alt="ecommerce"
-                        className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block"
-                        src="https://m.media-amazon.com/images/I/41pIShJzyJS._SY395_SX395_.jpg"
+                        className="m-auto h-[30vh] md:h-[36vh] block"
+                        src={products[item].img}
                       />
                     </a>
                     <div className="mt-4 text-center md:text-left">
@@ -27,10 +31,40 @@ const ShoulderBag = ({ products }) => {
                         SHOULDER BAG
                       </h3>
                       <h2 className="text-gray-900 title-font text-lg font-medium">
-                        Ladies Purse Handbag
+                        {products[item].title}
                       </h2>
-                      <p className="mt-1">₹499</p>
-                      <p className="mt-1">S, M, L, XL, XXL</p>
+                      <p className="mt-1">₹{products[item].price}</p>
+                      <div className="mt-1">
+                        {products[item].size.includes("S") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            S
+                          </span>
+                        )}
+                        {products[item].size.includes("M") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            M
+                          </span>
+                        )}
+                        {products[item].size.includes("L") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            L
+                          </span>
+                        )}
+                      </div>
+                      <div className="mt-1">
+                        {products[item].color.includes("Red") && (
+                          <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Green") && (
+                          <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Pink") && (
+                          <button className="border-2 border-gray-300 ml-1 bg-pink-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Blue") && (
+                          <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </Link>
@@ -48,10 +82,30 @@ export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
     await mongoose.connect(process.env.MONGO_URI);
   }
-  let products = await Product.find();
+  let products = await Product.find({ category: "shoulderbag" });
+  const shoulderbag = {};
+
+  products.forEach((item) => {
+    if (!shoulderbag[item.title]) {
+      shoulderbag[item.title] = {
+        ...item.toObject(),
+        color: [],
+        size: [],
+      };
+    }
+
+    if (item.availableQty > 0) {
+      if (!shoulderbag[item.title].color.includes(item.color)) {
+        shoulderbag[item.title].color.push(item.color);
+      }
+      if (!shoulderbag[item.title].size.includes(item.size)) {
+        shoulderbag[item.title].size.push(item.size);
+      }
+    }
+  });
 
   return {
-    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+    props: { products: JSON.parse(JSON.stringify(shoulderbag)) }, // will be passed to the page component as props
   };
 }
 
